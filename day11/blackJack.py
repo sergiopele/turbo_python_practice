e48c957427418a83008bf2b363fcf2daea9a504c
sergiopele <sergiypele@gmail.com>
2024-03-25 23:32:44 -0400

    day 11 - adding some functions to blackjack game

---
+++ day11/blackJack.py
@@ -0,0 +1,122 @@
+import random
+
+cpu = {
+        "player_name": "CPU", 
+        "cards": [],
+        "scores": 0,
+        "bidding": True
+        }
+user = {
+        "player_name": "Player", 
+        "cards":[],
+        "scores": 0,
+        "bidding": True,
+        }
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def reset_scores(): 
+        cpu["cards"] = []
+        cpu["scores"] = 0
+        cpu["bidding"] = True
+        user["cards"] = []
+        user["scores"] = 0
+        user["bidding"] = True
+
+def card_picker(is_first_round:bool, player:dict):
+        do_bidding = player["bidding"]
+        name = player["player_name"]
+        score = player["scores"]
+        if is_first_round:
+                for i in range(0,2):
+                        player["cards"].append(cards[random.randint(0, len(cards) - 1)])
+        else:        
+                player["cards"].append(cards[random.randint(0, len(cards) - 1)]) if do_bidding else print(f"{name} does not bidding, score: {score} ")
+
+def calculate_score(player:dict):
+        total_sum = sum(player["cards"])
+        def calculate_scores_when_over_21():
+                replace_11 = [1 if x == 11 else x for x in player["cards"]]
+                player["cards"] = replace_11
+                player["scores"] = sum(player["cards"])
+        def calculate_scores_when_below_21():
+                player["scores"] = total_sum
+        calculate_scores_when_over_21() if total_sum > 21 else calculate_scores_when_below_21()
+
+                
+def continue_game(player:dict):
+        score = player["scores"]
+        player_name = player["player_name"]
+        if score > 17 and player["bidding"] == True:
+                if player_name == "CPU":
+                        player["bidding"] = False
+                        print(f"{player_name} stops bidding. Score: {score}")
+                        return False
+                else:
+                        do_you_continue = input(f"Your current score is: {score}.\nDo you want to get another card? 'y' or 'n'")
+                        player["bidding"] = True if do_you_continue.lower() in "y" else False
+                        return player["bidding"]
+                        
+                
+def define_winner():
+        user_score = user["scores"]
+        cpu_scores = cpu["scores"]
+        user_bidding = user["bidding"]
+        cpu_bidding = cpu["bidding"]
+        is_winner_defined = False
+        if (not user_bidding and not cpu_bidding) or user_score > 21 or cpu_scores > 21:
+                if (user_score > 21 and cpu_scores < 21) or ((user_score < 21) < (cpu_scores < 22)):
+                        print("You LOSE")
+                        is_winner_defined = True
+                elif user_score < 22 and cpu_scores < 22:
+                        print("Draw")
+                        is_winner_defined = True
+                elif(user_score < 21 and cpu_scores > 21) or (user_score < 21 and cpu_scores > 21) or (user_score == 21 and cpu_scores != 21):
+                        print("You WIN!")
+                        is_winner_defined = True
+                else:
+                        is_winner_defined = False
+        else:
+                if user_score == 21:
+                        print("You WIN!")
+                        is_winner_defined = True
+                elif cpu_scores == 21:
+                        print("You Lose")
+                        is_winner_defined = True
+                else:
+                        is_winner_defined = False
+        if is_winner_defined:
+                if input("Do you want to play again? 'y' or 'n'?").lower() == "y":
+                        reset_scores()
+                        continue_playing = True
+                else:
+                       continue_playing = False 
+        
+                
+                
+
+
+
+        
+
+              
+continue_playing = False
+while not continue_playing:
+        card_picker(True, cpu)
+        card_picker(True, user)
+        calculate_score(user)
+        calculate_score(cpu)
+        print(cpu["scores"])
+        print(user["scores"])
+
+        while not define_winner():
+                continue_game(cpu)
+                continue_game(user)
+                card_picker(False, cpu)
+                card_picker(False, user)
+                calculate_score(user)
+                calculate_score(cpu)
+                print(cpu)
+                print(user)
+
+
