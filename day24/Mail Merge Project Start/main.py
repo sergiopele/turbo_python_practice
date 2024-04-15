982bc7c36cb8d24f1d52eabb4af9cf2e62c5801f
sergiopele <sergiypele@gmail.com>
2024-04-14 23:10:19 -0400

    adding some enhancements

--- day24/Mail Merge Project Start/main.py
+++ day24/Mail Merge Project Start/main.py
@@ -1,12 +1,8 @@
-# TODO: Create a letter using starting_letter.txt
-# for each name in invited_names.txt
-# Replace the [name] placeholder with the actual name.
-# Save the letters in the folder "ReadyToSend".
-
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-
+NAMES_FILE_PATH = "day24/Mail Merge Project Start/Input/Names/invited_names.txt"
+LETTER_FILE_PATH = "day24/Mail Merge Project Start/Input/Letters/starting_letter.txt"
+FILE_PATH_TO_CREATE_LETTER = (
+    "day24/Mail Merge Project Start/Output/ReadyToSend/example.txt"
+)
 
 def file_reader(file_path: str) -> list:
     with open(file_path, mode="r") as file_to_read:
@@ -21,12 +17,6 @@ def write_file(
         file_to_save.write(input_data)
 
 
-NAMES_FILE_PATH = "day24/Mail Merge Project Start/Input/Names/invited_names.txt"
-LETTER_FILE_PATH = "day24/Mail Merge Project Start/Input/Letters/starting_letter.txt"
-FILE_PATH_TO_CREATE_LETTER = (
-    "day24/Mail Merge Project Start/Output/ReadyToSend/example.txt"
-)
-# read names
 names: list = file_reader(NAMES_FILE_PATH)
 letter: list = file_reader(LETTER_FILE_PATH)
 
@@ -34,8 +24,10 @@ for name in names:
     composed_letter = ""
     for line in letter:
         if "[name]" in line:
-            composed_letter += line.replace("[name],", name.strip() + ',')
-            print(composed_letter)
+            composed_letter += line.replace("[name],", name.strip() + ",")
         else:
             composed_letter += line
-    write_file(f"day24/Mail Merge Project Start/Output/ReadyToSend/{name}.txt", composed_letter.strip())
+    write_file(
+        f"day24/Mail Merge Project Start/Output/ReadyToSend/letter_for_{name}.txt",
+        composed_letter.strip(),
+    )
